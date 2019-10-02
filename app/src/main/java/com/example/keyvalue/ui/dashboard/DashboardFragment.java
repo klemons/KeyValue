6d8c8b453c3d0f844caf222a37902a7bfafb5be6
Kyle Lemons <klemons15@hotmail.com>
2019-10-02 10:09:10 -0500

    implemented a 10 second timer that starts when the button is first pressed

--- app/src/main/java/com/example/keyvalue/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/keyvalue/ui/dashboard/DashboardFragment.java
@@ -3,6 +3,7 @@ package com.example.keyvalue.ui.dashboard;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,15 +12,21 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.keyvalue.R;
 
+import java.util.regex.Matcher;
+
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private boolean isTimerRunning;
+    private CountDownTimer countDownTimer;
+    public TextView textTime;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +34,7 @@ public class DashboardFragment extends Fragment {
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
+        textTime = root.findViewById(R.id.textTime);
         dashboardViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -34,13 +42,19 @@ public class DashboardFragment extends Fragment {
             }
         });
 
-        Button button = root.findViewById(R.id.mash_button);
+        Button button = root.findViewById(R.id.mash_button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (isTimerRunning) {
+
+                }
+                else {
+                    startTimer();
+                }
                 SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-                int defaultValue = getResources().getInteger(R.integer.saved_times_pressed_default_key);
-                int timesPressed = sharedPref.getInt(getString(R.string.saved_button_press_count_key), defaultValue);
+                int currentScore = getResources().getInteger(R.integer.current_Score);
+                int timesPressed = sharedPref.getInt(getString(R.string.saved_button_press_count_key), currentScore);
 
                 int newTimesPressed = timesPressed + 1;
 
@@ -52,7 +66,23 @@ public class DashboardFragment extends Fragment {
             }
         });
 
-
         return root;
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                isTimerRunning = false;
+                textTime.setText("Finished!");
+            }
+        }.start();
+
+        isTimerRunning = true;
     }
 }
